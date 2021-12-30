1ee990170cacc42ecf124d6a5b732345a289e111
AlexPi <alex.pigida@outlook.com>
2021-12-29 22:32:42 -0500

    pau

--- Src/PowerShellLog/Helpers/FSHelper.cs
+++ Src/PowerShellLog/Helpers/FSHelper.cs
@@ -1,38 +1,34 @@
-﻿using AAV.Sys.Ext;
-using System;
+﻿using System;
 using System.IO;
+using AAV.Sys.Ext;
 
-namespace PowerShellLog.Helpers
+namespace PowerShellLog.Helpers;
+
+public static class FSHelper
 {
-  public static class FSHelper
+  public static string GetCreateSafeLogFolderAndFile(string[] fullPaths)
   {
-    public static bool ExistsOrCreated(string folder) // true if created or exists; false if unable to create.
+    foreach (var fp in fullPaths)
+      if (ExistsOrCreated(Path.GetDirectoryName(fp) ?? throw new ArgumentNullException(nameof(fullPaths))))
+        return fp;
+
+    return "__FallbackName__.log";
+  }
+
+  public static bool ExistsOrCreated(string folder) // true if created or exists; false if unable to create.
+  {
+    try
     {
-      try
-      {
-        if (Directory.Exists(folder))
-          return true;
+      if (Directory.Exists(folder))
+        return true;
 
-        Directory.CreateDirectory(folder);
+      Directory.CreateDirectory(folder);
 
-        return Directory.Exists(folder);
-      }
-      catch (IOException ex) { ex.Log($"Directory.CreateDirectory({folder})"); }
-      catch (Exception ex) { ex.Log($"Directory.CreateDirectory({folder})"); throw; }
-
-      return false;
+      return Directory.Exists(folder);
     }
+    catch (IOException ex) { ex.Log($"Directory.CreateDirectory({folder})"); }
+    catch (Exception ex) { ex.Log($"Directory.CreateDirectory({folder})"); throw; }
 
-    public static string GetCreateSafeLogFolderAndFile(string[] fullPaths)
-    {
-      //if (Environment.UserName == "apigida")
-      //  return @$"..\..\Logs\{Path.GetFileName(fullPaths[0])}";
-
-      foreach (var fp in fullPaths)
-        if (ExistsOrCreated(Path.GetDirectoryName(fp)))
-          return fp;
-
-      return "__FallbackName__.log";
-    }
+    return false;
   }
 }
